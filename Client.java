e8f4898119a48b438cfe410da4c512ef76c9630e
PatrickKallenbach <pkallenbach1@ufl.edu>
2025-02-13 21:42:43 -0500

    file read and write within client

--- Client.java
+++ Client.java
@@ -2,6 +2,8 @@ import java.net.*;
 import java.io.*;
 import java.nio.*;
 import java.nio.channels.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 // Patrick Kallenbach - CNT4007 Project 1
@@ -66,9 +68,76 @@ public class Client {
 						case "upload":
 							System.out.println("UPLOADING FILE " + filename);
 
-							sendMessage("UPLOAD " + filename);
+							int packetSize = 1000;
 
+							sendMessage("UPLOAD " + filename);
 							confirm = (String)in.readObject();
+
+							SortedMap<Long, byte[]> storedFile = new TreeMap<>();
+
+							// Prepare input file
+							FileInputStream infile = new FileInputStream(filename);
+
+							// Prepare tracking values
+							long track = 0;
+							long fileLength = infile.getChannel().size();
+							long remainingFileLength = fileLength;
+
+							while (remainingFileLength > 0) {
+								// Define Header information
+									// (Packet #, Total Packet #, Packet Length)
+								ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES*3); // Create a 24-byte buffer
+								
+								// Place tracking values in header
+								buffer.putLong(track);
+								buffer.putLong(fileLength / packetSize);
+								buffer.putLong(Math.min(packetSize, remainingFileLength));
+								buffer.flip(); // Flip to read header in correct order
+
+								byte[] header = buffer.array();
+
+								// Read data into temporary storage
+								byte[] data = new byte[packetSize];
+								infile.read(data);
+
+								// Write header and data into packet
+								byte[] packet = new byte[packetSize + 24];
+
+								// Copy header and data to packet
+								System.arraycopy(header, 0, packet, 0, 24);
+								System.arraycopy(data, 0, packet, 24, packetSize);
+
+								// Insert packet into storedFile map
+								storedFile.put(track, packet);
+
+								// update tracking counters
+								track += 1;
+								remainingFileLength -= packetSize;
+							}
+
+							// Create file to write new content to
+							FileOutputStream outfile = new FileOutputStream("newUploadTestFile.pptx");
+
+							// For each entry now held in storedFile, write to outfile
+							for (SortedMap.Entry<Long, byte[]> receivePacket : storedFile.entrySet()) {
+
+								// Create new bytebuffer with information from each packet
+								ByteBuffer receiveData = ByteBuffer.wrap(receivePacket.getValue());
+								long packetNumber = receiveData.getLong();
+								long totalPackets = receiveData.getLong();
+								int packetLength = (int)receiveData.getLong(); // important value from buffer, determines packet length
+
+								// Prepare array to write to file
+								byte[] writeOut = new byte[packetLength];
+								receiveData.get(writeOut, 0, packetLength);
+
+								// write character by character to outfile
+								for (byte character : writeOut) {
+									outfile.write(character);
+								}
+							}
+
+							outfile.close();
 
 							sendMessage("DONE");
 							
@@ -81,6 +150,8 @@ public class Client {
 									
 							confirm = (String)in.readObject();
 							System.out.println("File uploaded.");
+							
+							out.flush();
 
 							break;
 						default:
